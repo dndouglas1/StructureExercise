444f322351e17e1883d4a000b7c53d09fac1e1e1
dndouglas1 <67969248+dndouglas1@users.noreply.github.com>
2020-07-29 07:45:49 -0500

    Add files via upload

---
+++ Deshaun_D_StructureExercise.playground/Contents.swift
@@ -0,0 +1,10 @@
+struct  CarEngine {
+    var V4Eng = 147
+    var V6Eng = 235
+    var V8Eng = 324
+}
+let myCarEngine = CarEngine()
+
+print("The price of the V4 Engine is $\(myCarEngine.V4Eng).")
+print("The price of the V6 Engine is $\(myCarEngine.V6Eng).")
+print("The price of the V8 Engine is $\(myCarEngine.V8Eng).")
